2d6c6f7d258957fa6603a3ef0dd0e85bea45733e
madamcin <adamcin@gmail.com>
2013-12-24 09:23:12 -0800

    renaming ning client namespace

--- http-helpers/src/main/java/net/adamcin/httpsig/http/ning/RotateAndReplayResponseFilter.java
+++ http-helpers/src/main/java/net/adamcin/httpsig/http/ning/RotateAndReplayResponseFilter.java
@@ -25,7 +25,7 @@
  * For more information, please refer to <http://unlicense.org/>
  */
 
-package net.adamcin.httpsig.http.async;
+package net.adamcin.httpsig.http.ning;
 
 import com.ning.http.client.FluentCaseInsensitiveStringsMap;
 import com.ning.http.client.Request;
