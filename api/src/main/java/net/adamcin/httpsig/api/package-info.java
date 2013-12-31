9ef82cffb1e79200a2fd76ff187b06d790d68f7e
Mark Adamcin <adamcin@gmail.com>
2013-12-30 16:27:47 -0800

    updated copyright; added package-info.java

--- api/src/main/java/net/adamcin/httpsig/api/package-info.java
+++ api/src/main/java/net/adamcin/httpsig/api/package-info.java
@@ -25,16 +25,7 @@
  * For more information, please refer to <http://unlicense.org/>
  */
 
+@Version("1.0.2")
 package net.adamcin.httpsig.api;
 
-/**
- * Interface used by a {@link Keychain} to generate keyId's from the {@link Key}s that it contains
- */
-public interface KeyId {
-
-    /**
-     * @param key the {@link Key} to identify
-     * @return the generated keyId or null if the {@link Key} cannot be identified
-     */
-    String getId(Key key);
-}
+import aQute.bnd.annotation.Version;
